f49da097c72520dd6ef19240851203952f5c2429
evandro663 <evaandro@msn.com>
2023-02-12 15:41:30 -0300

    fix: ajustes interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,20 +1,46 @@
 export interface ILogin {
-  email: string,
-  password:string
+  email: string;
+  password: string;
 }
 
 export interface IJwt {
   data: {
-    email: string,
-    password: string,
-  }
+    email: string;
+    password: string;
+  };
 }
 
 export interface ISolicitacao {
+  id: number;
+  tipo_servico: string;
+  created_at: Date;
+  coordenadas_origem: string;
+  valor_total: number;
+  distancia_total: number;
+  tempo_total: number;
+  empresa: string;
+}
 
-  tipoServico: string,
-  valorTotal: number,
-  distanciaTotal: number,
-  tempoTotal: number,
-  empresa: string
+export interface IVeiculo {
+  placa: string;
+  modelo: string;
+  marca: string;
+  ano: number;
+  coordenadas_entrega: string;
+  endereco_entrega: string;
+  distancia_total: number;
+  duracao_total: number;
+  ordem_de_entrega: number;
+}
+
+export interface IData {
+  Solicitacao: ISolicitacao;
+  Veiculos: IVeiculo[];
+}
+
+export interface TiposServicos {
+  [key: string]: {
+    valorFixo: number;
+    valorPorKm: number;
+  };
 }
