e9d59290cea7216dbe722cf453040c5d7d3d1e03
evandro663 <evaandro@msn.com>
2023-02-12 15:40:10 -0300

    feat: Adiciona controller e suas respectivas funcoes

--- src/controllers/solicitacaoController.ts
+++ src/controllers/solicitacaoController.ts
@@ -1,17 +1,33 @@
 import { Request, Response } from 'express';
 import solicitacaoService from '../services/solicitacaoService';
+import veiculoService from '../services/veiculoService';
 
 const solicitacaoController = {
-  async createSolicitacao(req: Request, res: Response) {
-    const data = await solicitacaoService.createSolicitacao(req.body);
-    // await loginService.validateUserByEmail(data.email);
-    // const token = await loginService.makeToken(req.body);
+  async add(req: Request, res: Response) {
+    const request = req.body;
+    await solicitacaoService.validateDefaultBody(request);
+    await solicitacaoService.validateBodySolicitacao(request.Solicitacao);
+    await veiculoService.validateBodyVeiculo(request.Veiculos);
+    const data = await solicitacaoService.add(request);
+    return res.status(200).json({
+      message: 'Solicitação criada com sucesso!',
+      Solicitacao: data[0],
+      Veiculos: data[1]
+    });
+
+  },
+
+  async listById(req: Request, res: Response) {
+    const params: number = parseInt(req.params.id);
+    const data: any = await solicitacaoService.listById(params);
     return res.status(200).json(data);
   },
 
-  async list(req: Request, res: Response) {
-    const solicitacao = await solicitacaoService.list();
-    return res.status(200).json(solicitacao);
+  async listByCompany(req: Request, res: Response) {
+    const request = req.body;
+    await solicitacaoService.validateBodyLista(request);
+    const data = await solicitacaoService.listByCompany(request.empresa, request.start_date, request.end_date);
+    return res.status(200).json(data);
   }
 
 };
