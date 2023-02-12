f3b8d70c903148b9f0fb07ef108334442b981dfd
evandro663 <evaandro@msn.com>
2023-02-12 15:41:58 -0300

    feat: adiciona novas rotas

--- src/routes/solicitacaoRoute.ts
+++ src/routes/solicitacaoRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import solicitacaoController from '../controllers/solicitacaoController';
+import tokenValidationMiddleware from '../Middlewares/tokenValidationMiddleware';
 
 const solicitacaoRoute = Router();
 
-solicitacaoRoute.post('/', solicitacaoController.createSolicitacao);
-solicitacaoRoute.get('/', solicitacaoController.list);
+solicitacaoRoute.post('/', tokenValidationMiddleware, solicitacaoController.add);
+solicitacaoRoute.get('/:id', tokenValidationMiddleware, solicitacaoController.listById);
+solicitacaoRoute.post('/detalhes', tokenValidationMiddleware, solicitacaoController.listByCompany);
 
 export default solicitacaoRoute;
