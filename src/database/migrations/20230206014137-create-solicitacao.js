0d9c21f2b8efe3c3502e71e0f62366b446457290
evandro663 <evaandro@msn.com>
2023-02-12 15:40:26 -0300

    feat: Cria migrations

---
+++ src/database/migrations/20230206014137-create-solicitacao.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('solicitacoes', {
+      id: { 
+        type: Sequelize.INTEGER,
+        allowNull: false, 
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      tipo_servico: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      coordenadas_origem: {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      },
+      valor_total: {
+        type: Sequelize.DECIMAL(15, 2),
+      },
+      distancia_total: {
+        type: Sequelize.INTEGER
+      },
+      tempo_total: {
+        type: Sequelize.INTEGER
+      },
+      empresa: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('solicitacoes');
+  }
+};
